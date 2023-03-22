2dcf30f090cdb0f9435a91c14653ee7751105cb3
Rexx Alfaro <rexxalfaro+github@gmail.com>
2023-03-21 17:44:08 -0700

    Reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function reverseString(text) {
+    let word = '';
+    for (let i = text.length; i > 0; i--){
+        word += text[i - 1];
+    }
+    return word;
 };
+
+/* BEST ANSWER:
+const reverseString = function (string) {
+  return string.split("").reverse().join("");
+};
+
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
