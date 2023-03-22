383072888535f7680997ec6a946aed25932c5e95
Rexx Alfaro <rexxalfaro+github@gmail.com>
2023-03-21 17:14:01 -0700

    Repeat a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let word = '';
+    for (let i = 0; i < num; i++){
+        word += string;
+        
+    }
+    return word;
 };
-
+// string = string + string;
 // Do not edit below this line
 module.exports = repeatString;
