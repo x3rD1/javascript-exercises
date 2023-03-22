7e49cce65fd4f4bf772ef85f4f7c7ba08fa5faf9
Rexx Alfaro <rexxalfaro+github@gmail.com>
2023-03-21 18:59:57 -0700

    Remove an element from the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function remove(array,n) {
+    array.splice(array.indexOf(n), 1);
+    return array;
 };
 
 // Do not edit below this line
